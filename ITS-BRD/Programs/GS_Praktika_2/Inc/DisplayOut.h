7df9b0d2e4bc65223cb2b71cbd69a6c7a7b0d524
SmitYogeshbhaiMoradiya <128420826+SmitYogeshbhaiMoradiya@users.noreply.github.com>
2024-05-13 18:41:25 +0200

    Final

---
+++ ITS-BRD/Programs/GS_Praktika_2/Inc/DisplayOut.h
@@ -0,0 +1,11 @@
+#ifndef _DISPLAYOUT_H
+#define	_DISPLAYOUT_H
+#include "error_handeling.h"
+#include "LCD_GUI.h"
+#include "lcd.h"
+
+int init_display();
+
+int setValues(int Winkel,int Winkelgeschwindigkeit);
+
+#endif 
---
+++ ITS-BRD/Programs/GS_Praktika_2/Src/DisplayOut.c
@@ -0,0 +1,26 @@
+#include "DisplayOut.h"
+
+
+int init_display(){
+	
+	lcdGotoXY( 0, 0);
+	lcdPrintS("Drehwinkel:");
+	lcdGotoXY( 0, 10);
+ 	lcdPrintS("Winkelg. ");
+	
+	return SUCCESS;
+	}
+
+	int setValues(int Winkel,int Winkelgeschwindigkeit){
+	lcdGotoXY( 70, 0);
+	lcdPrintInt(Winkel);
+	lcdGotoXY( 70, 10);
+	lcdPrintInt(Winkelgeschwindigkeit);
+	
+	}
+
+
+
+
+
+
