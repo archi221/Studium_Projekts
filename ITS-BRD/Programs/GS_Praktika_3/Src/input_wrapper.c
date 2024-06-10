d8225fdc1b3cd204c70173764602e3932d0a2881
archi221 <timon.guenther@mail.de>
2024-06-10 15:44:28 +0200

    get info header, pallete, file header

--- ITS-BRD/Programs/GS_Praktika_3/Src/input_wrapper.c
+++ ITS-BRD/Programs/GS_Praktika_3/Src/input_wrapper.c
@@ -1,13 +1,42 @@
 #include "input_wrapper.h"
-#include <stdlib>
+#include <stdio.h>
+#include <stdbool.h>
+#include "errorhandler.h"
+#include "input.h"
 
+void get_BM_file_header() {
+    openNextFile();
+    ERR_HANDLER(true != COMread((char *) &BITMAPINFOHEADER,
+                                sizeof(BITMAPFILEHEADER), 1),
+                "readFileHeaders: Error during read.");
+    ERR_HANDLER(fileHeader != NULL, "readFileHeaders: Nullpointer");
+}
 
-void get_BM_file_header(struct tagBITMAPFILEHEADER *file_header);
+void get_BM_info_header() {
+    ERR_HANDLER(true != COMread((char *) &BITMAPFILEHEADER,
+                                sizeof(BITMAPINFOHEADER), 1),
+                "readInfoHeader: Error during read.");
+    ERR_HANDLER(info_header != NULL, "readInfoHeader: Nullpointer");
+    ERR_HANDLER(&BITMAPFILEHEADER->bfOffBits == sizeof(file_header) + sizeof(info_header),
+                "readInfoHeader: wrong offsett");
+}
 
-void get_BM_info_header(struct tagBITMAPINFOHEADER *info_header);
+void get_BT_palette(struct tagRGBQUAD palette[]) {
+    ERR_HANDLER(true != COMread((char *) palette,
+                                sizeof(RGBQUAD) * 266, 1),
+                "readInfoHeader: Error during read.");
+}
 
-void get_BT_palette(struct tagRGBQUAD[]);
+void get_next_line_8compressed(char *line, bool compressed) {
+    if (compressed) {
+        decompress_line(line)
+    }
+}
 
-void get_next_line(char *line);
+void get_next_line_24compressed(char *line, bool compressed) {
+    ERR_HANDLER(compressed, "wrong format for compressed flag in InfoHeader")
+}
 
-void decompress_line(char *line);
+void decompress_line(char *line) {
+
+}
