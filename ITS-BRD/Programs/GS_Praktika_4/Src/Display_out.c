d1bab6fd022f57c891996bed9026bf3700d9b11d
archi221 <98173934+archi221@users.noreply.github.com>
2024-07-03 21:58:53 +0200

    delte unused sensor

--- ITS-BRD/Programs/GS_Praktika_4/Src/Display_out.c
+++ ITS-BRD/Programs/GS_Praktika_4/Src/Display_out.c
@@ -5,7 +5,9 @@
 #include <inttypes.h>
 #include <string.h>
 
-char buffer[30];
+static uint64_t lastpdroms[5];
+static int last_anzahl = 0;
+static char buffer[30];
 
 void init_display() {
     GUI_init(DEFAULT_BRIGHTNESS);
@@ -20,24 +22,32 @@ void init_display() {
 void print_sensoren(sensor *sensoren, int anzahl) {
     uint64_t pdrom = 0;
     for (int i = 0; i < anzahl; ++i) {
-        lcdGotoXY(0, i + 1);
-        if (sensoren[i].pdrom[0] == 0x28) {
-            strncpy(buffer, "DS18B20", 7);
-        } else if (sensoren[i].pdrom[0] == 0x10) {
-            strncpy(buffer, "DS18S20", 7);
-        } else {
-            strncpy(buffer, "unknown", 7);
-        }
-        lcdPrintS(buffer);
-        lcdGotoXY(9, i + 1);
-        for (int j = 0; j < 8; ++j) {
+			  for (int j = 0; j < 8; ++j) {
             pdrom |= ((uint64_t) sensoren[i].pdrom[7 - j] << (j * 8));
         }
-        sprintf(buffer, "0x%"
-        PRIx64, pdrom);
+				if (lastpdroms[i] != pdrom) {
+					lcdGotoXY(0, i + 1);
+					if (sensoren[i].pdrom[0] == 0x28) {
+							strncpy(buffer, "DS18B20", 7);
+					} else if (sensoren[i].pdrom[0] == 0x10) {
+							strncpy(buffer, "DS18S20", 7);
+					} else {
+							strncpy(buffer, "unknown", 7);
+					}
+					lcdPrintS(buffer);
+					lcdGotoXY(9, i + 1);
+					sprintf(buffer, "0x%"PRIx64, pdrom);
+			}
         lcdPrintS(buffer);
         snprintf(buffer, 9, "%3.6f", sensoren[i].temperatur);
         lcdGotoXY(30, i + 1);
         lcdPrintS(buffer);
     }
+		if (last_anzahl > anzahl) {
+			for (int i = 0; i < (last_anzahl - anzahl); ++i) {
+				lcdGotoXY(0, i + anzahl + 1);
+				lcdPrintlnS("                        ");
+			}
+		}
+		last_anzahl = anzahl;
 }
